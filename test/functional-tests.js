54cf5c77abcc29a3a74dcbe2c0cab0c9091e7379
Henry Cuffy <hcuffy_5@hotmail.com>
2018-08-08 07:30:36 +0200

    added test

--- test/functional-tests.js
+++ test/functional-tests.js
@@ -1,65 +1,31 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const assert = require('chai').assert
-
 const server = require('../server')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
 chai.use(chaiHttp)
 
-describe('tests', () =>  {
-	it('All books should return all books', (done) =>  {
+describe('functional test', () =>  {
+// it('should get all books', (done) =>  {
+// 	chai.request(server)
+// 		.post('/new/book/')
+// 		.set('content-type', 'application/x-www-form-urlencoded')
+// 		.send({ title : 'New book of dreams' })
+// 		.end((err, res) => {
+// 			assert.equal(res.status, 200)
+// 			done()
+// 		})
+// })
+
+	it('should get all books', (done) =>  {
 		chai.request(server)
 			.get('/all-books/')
 			.end((err, res) => {
-				assert.equal(res.status, 200)
-				done()
-			})
-	})
-
-	it('Get single book', (done) =>  {
-		chai.request(server)
-			.get('/single-book/')
-			.query({ id: '5b521096a4ce3b6071b138a6'  })
-			.end((err, res) => {
-				assert.equal(res.status, 200)
-				done()
-			})
-	})
-
-it('A book should be added', (done) =>  {
-	chai.request(server)
-		.post('/new/book/')
-		.set('content-type', 'application/x-www-form-urlencoded')
-		.send({ title: 'test book', comment: ['test book'] })
-		.end((err, res) => {
-			assert.equal(res.status, 200)
-			done()
-		})
-})
-
-	it('Should add comment', (done) =>  {
-		chai.request(server)
-			.post('/add-comment/5b52108ea4ce3b6071b138a4')
-			.set('content-type', 'application/x-www-form-urlencoded')
-			.send({ comment: ['test book'] })
-			.end((err, res) => {
-				assert.equal(res.status, 200)
-				done()
-			})
-	})
-
-	it('Book should not be removed', (done) =>  {
-		chai.request(server)
-			.delete('/remove/aaaa108ea4ce3b6071b13fff')
-			.end((err, res) => {
-				assert.equal(res.text, 'Could not find book for deletion')
-				done()
-			})
-	})
-
-	it('Remove all books', (done) =>  {
-		chai.request(server)
-			.delete('/remove-all/')
-			.end((err, res) => {
+				console.log(res.text)
+				const dom = new JSDOM(res.text)
+				let output = dom.window.document.body.querySelector('.all-books').textContent
 				assert.equal(res.status, 200)
 				done()
 			})
